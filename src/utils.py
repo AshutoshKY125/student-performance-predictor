3060d41d89c6b11723858be7ba1bcfc9a3857908
AshutoshKY125 <ashutoshkumaryadav040903@gmail.com>
2025-05-21 19:02:05 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pickle
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging  
 
@@ -28,3 +29,32 @@ def save_object(file_path, obj):
             logging.info(f"Object saved at {file_path}")
     except Exception as e:
         raise CustomException(e, sys) 
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    This function evaluates the performance of different regression models.
+    
+    Parameters:
+    X_train (array-like): Training feature set.
+    y_train (array-like): Training target variable.
+    X_test (array-like): Testing feature set.
+    y_test (array-like): Testing target variable.
+    models (dict): Dictionary of models to be evaluated.
+    
+    Returns:
+    dict: A dictionary containing the model names and their respective scores.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+        
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)   
